aed1dfcf572134a7e704ec6877ba86ab38ef56f2
Sofia Kovaleva <kov.sfa@gmail.com>
2025-08-12 21:17:22 +0500

    make Faker test data random for each test method

--- src/test/java/tests/AutomationPracticeFormPageObjectAndFakerTest.java
+++ src/test/java/tests/AutomationPracticeFormPageObjectAndFakerTest.java
@@ -1,42 +1,74 @@
 package tests;
 
 import base.TestBase;
+import data.UserTestData;
 import org.junit.jupiter.api.*;
 import pages.RegistrationPage;
-
-import static data.TestValue.*;
 
 public class AutomationPracticeFormPageObjectAndFakerTest extends TestBase {
 
   RegistrationPage registrationPage = new RegistrationPage();
+  UserTestData u;
+
+  @BeforeEach
+  void setupData() {
+    u = UserTestData.getInstance();
+  }
 
   @Test
   void successfulRegistrationTestWithFakerData() {
     registrationPage.openPage()
-        .setFirstName(firstName)
-        .setLastName(lastName)
-        .setEmail(userEmail)
-        .setGender(userGender)
-        .setPhoneNumber(userPhoneNumber)
-        .setBirthDate(userBirthDate)
-        .setSubjects(userSubjects)
-        .setHobbies(userHobby)
-        .uploadPicture(userPictureFilePath)
-        .setCurrentAddress(userAddress)
-        .setState(userState)
-        .setCity(userCity)
+        .setFirstName(u.firstName)
+        .setLastName(u.lastName)
+        .setEmail(u.email)
+        .setGender(u.gender)
+        .setPhoneNumber(u.phoneNumber)
+        .setBirthDate(u.dateOfBirth)
+        .setSubjects(u.subject)
+        .setHobbies(u.hobby)
+        .uploadPicture(u.pictureFilePath)
+        .setCurrentAddress(u.address)
+        .setState(u.state)
+        .setCity(u.city)
         .submit();
 
     // Checks
-    registrationPage.checkResult("Student Name", firstName + " " + lastName)
-        .checkResult("Student Email", userEmail)
-        .checkResult("Gender", userGender)
-        .checkResult("Mobile", userPhoneNumber)
-        .checkResult("Date of Birth", userBirthDateFormatted)
-        .checkResult("Subjects", userSubjects)
-        .checkResult("Hobbies", userHobby)
-        .checkResult("Picture", userPictureFileName)
-        .checkResult("Address", userAddress)
-        .checkResult("State and City", userState + " " + userCity);
+    registrationPage.checkResult("Student Name", u.firstName + " " + u.lastName)
+        .checkResult("Student Email", u.email)
+        .checkResult("Gender", u.gender)
+        .checkResult("Mobile", u.phoneNumber)
+        .checkResult("Date of Birth", u.dateOfBirthFormatted)
+        .checkResult("Subjects", u.subject)
+        .checkResult("Hobbies", u.hobby)
+        .checkResult("Picture", u.pictureFileName)
+        .checkResult("Address", u.address)
+        .checkResult("State and City", u.state + " " + u.city);
+  }
+
+  @Test
+  void minimalSuccessfulDataTest() {
+    registrationPage.openPage()
+        .setFirstName(u.firstName)
+        .setLastName(u.lastName)
+        .setGender(u.gender)
+        .setPhoneNumber(u.phoneNumber)
+        .submit();
+
+    // Checks
+    registrationPage.checkResult("Student Name", u.firstName + " " + u.lastName)
+        .checkResult("Gender", u.gender)
+        .checkResult("Mobile", u.phoneNumber);
+  }
+
+  @Test
+  void emptyNameNegativeTest() {
+    registrationPage.openPage()
+        .setLastName(u.lastName)
+        .setGender(u.gender)
+        .setPhoneNumber(u.phoneNumber)
+        .submit();
+
+    // Checks
+    registrationPage.checkFormWasValidatedButTableIsNotVisible();
   }
 }
