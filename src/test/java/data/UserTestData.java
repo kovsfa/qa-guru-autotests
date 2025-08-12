bc672f17fd044d287da0bda91ca3f10ee853c57c
Sofia Kovaleva <kov.sfa@gmail.com>
2025-08-12 21:46:38 +0500

    simplify UserTestData class (improve scalability)

--- src/test/java/data/UserTestData.java
+++ src/test/java/data/UserTestData.java
@@ -4,37 +4,24 @@ import java.util.Date;
 
 public class UserTestData {
 
-  public final String firstName, lastName, email, address, gender, phoneNumber;
-  public final Date dateOfBirth;
-  public final String dateOfBirthFormatted, subject, hobby, pictureFilePath, pictureFileName, state, city;
+  public final String firstName = TestDataGenerator.getFirstName();
+  public final String lastName = TestDataGenerator.getLastName();
+  public final String email = TestDataGenerator.getEmail();
+  public final String address = TestDataGenerator.getAddress();
+  public final String gender = TestDataGenerator.getGender();
+  public final String phoneNumber = TestDataGenerator.getPhoneNumber();
+  public final Date dateOfBirth = TestDataGenerator.getDateOfBirth();
+  public final String dateOfBirthFormatted = TestDataGenerator.formatDate(dateOfBirth);
+  public final String subject = TestDataGenerator.getSubject();
+  public final String hobby = TestDataGenerator.getHobby();
+  public final String pictureFilePath = TestDataGenerator.getFilePath();
+  public final String pictureFileName = TestDataGenerator.getFileName(pictureFilePath);
+  public final String state = TestDataGenerator.getState();
+  public final String city = TestDataGenerator.getCity(state);
 
-  private UserTestData(String firstName, String lastName, String email, String address, String gender,
-      String phoneNumber, Date dateOfBirth, String dateOfBirthFormatted, String subject,
-      String hobby, String pictureFilePath, String pictureFileName, String state, String city) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.address = address;
-    this.gender = gender;
-    this.phoneNumber = phoneNumber;
-    this.dateOfBirth = dateOfBirth;
-    this.dateOfBirthFormatted = dateOfBirthFormatted;
-    this.subject = subject;
-    this.hobby = hobby;
-    this.pictureFilePath = pictureFilePath;
-    this.pictureFileName = pictureFileName;
-    this.state = state;
-    this.city = city;
-  }
+  private UserTestData() {}
 
   public static UserTestData getInstance() {
-    Date dateOfBirth = TestDataGenerator.getDateOfBirth();
-    String filePath = TestDataGenerator.getFilePath();
-    String state = TestDataGenerator.getState();
-    return new UserTestData(TestDataGenerator.getFirstName(), TestDataGenerator.getLastName(),
-        TestDataGenerator.getEmail(), TestDataGenerator.getAddress(), TestDataGenerator.getGender(),
-        TestDataGenerator.getPhoneNumber(), dateOfBirth, TestDataGenerator.formatDate(dateOfBirth),
-        TestDataGenerator.getSubject(), TestDataGenerator.getHobby(), filePath,
-        TestDataGenerator.getFileName(filePath), state, TestDataGenerator.getCity(state));
+    return new UserTestData();
   }
 }
